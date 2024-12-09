beb85f8a64ac39148b87b4bd6466fe699360fa3f
김건학 <kkh10002@naver.com>
2024-12-09 23:07:49 +0900

    feat(InputView): 입력 뷰 기능 구현
    자동차 이름을 입력받는 기능을 구현했습니다.
    게임 시도 횟수를 입력받는 기능을 구현했습니다.

---
+++ java-racingcar-7/src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,34 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class InputView {
+    private static final String NUMBER_REGEX = "\\d+";
+    public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        validateNull(carNames);
+        return carNames;
+    }
+
+    public int inputAttemptCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String attemptCount = Console.readLine();
+        validateNumber(attemptCount);
+        return Integer.parseInt(attemptCount);
+    }
+
+    private void validateNull(final String input) {
+        Optional.ofNullable(input)
+                .orElseThrow(() -> new IllegalArgumentException("null을 입력하셨습니다.");
+    }
+
+    private void validateNumber(final String attemptCount) {
+        validateNull(attemptCount);
+        if(!Pattern.matches(NUMBER_REGEX, attemptCount)){
+            throw new IllegalArgumentException("잘못된 형식의 숫자를 입력했습니다.");
+        }
+    }
+}
