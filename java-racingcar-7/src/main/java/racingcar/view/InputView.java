8028c5aaa864960e0093f09081ca50b092ec7466
김건학 <kkh10002@naver.com>
2024-12-09 23:09:00 +0900

    fix(InputView): 괄호 누락 오류 수정

--- java-racingcar-7/src/main/java/racingcar/view/InputView.java
+++ java-racingcar-7/src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class InputView {
     private static final String NUMBER_REGEX = "\\d+";
+
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
@@ -22,12 +23,12 @@ public class InputView {
 
     private void validateNull(final String input) {
         Optional.ofNullable(input)
-                .orElseThrow(() -> new IllegalArgumentException("null을 입력하셨습니다.");
+                .orElseThrow(() -> new IllegalArgumentException("null을 입력하셨습니다."));
     }
 
     private void validateNumber(final String attemptCount) {
         validateNull(attemptCount);
-        if(!Pattern.matches(NUMBER_REGEX, attemptCount)){
+        if (!Pattern.matches(NUMBER_REGEX, attemptCount)) {
             throw new IllegalArgumentException("잘못된 형식의 숫자를 입력했습니다.");
         }
     }
