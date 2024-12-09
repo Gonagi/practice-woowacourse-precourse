2c7a26ed39022891352f7cd65b4889119ee14b8b
김건학 <kkh10002@naver.com>
2024-12-10 00:08:24 +0900

    refactor(OutputView): 출력 기능 서비스 삭제
    다른 객체를 주입받지 않으므로 출력서비스 삭제

--- java-racingcar-7/src/main/java/racingcar/view/OutputView.java
+++ java-racingcar-7/src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class OutputView {
     public void printResultMessage() {
         System.out.println("실행 결과");
+    }
+
+    public void printEachResult(final List<Car> cars) {
+        for (Car car : cars) {
+            System.out.printf("%s : %s\n", car.getName(), car.getLocation());
+        }
+        System.out.println();
     }
 
     public void printWinners() {
