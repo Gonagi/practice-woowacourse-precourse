dbe758827a6e47c119e446f0294d16eec7b1d3da
김건학 <kkh10002@naver.com>
2024-12-12 17:24:41 +0900

    feat(util): 파일에서 읽어오는 기능 구현
    파일 경로에서 파일을 읽어 문자열 리스트로 반환한다.
    문자열을 읽어 프로모션을 생성한다.
    문자열을 읽어 상품을 생성한다.
    final 클래스로 선언하여 상속을 방지한다.

---
+++ java-convenience-store-7/src/main/java/store/util/FileUtils.java
@@ -0,0 +1,54 @@
+package store.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import store.domain.product.Product;
+import store.domain.product.Product.Builder;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.Promotions;
+
+public final class FileUtils {
+    private static final String COMMA = ",";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private FileUtils() {
+
+    }
+
+    public static List<String> readLinesFromFile(final String filePath) throws FileNotFoundException {
+        ArrayList<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+        }
+        return lines;
+    }
+
+    public static Promotion lineToPromotion(final String line) {
+        String[] promotionInfo = line.split(COMMA);
+        String name = promotionInfo[0];
+        int buy = Integer.parseInt(promotionInfo[1]);
+        int get = Integer.parseInt(promotionInfo[2]);
+        LocalDate startDate = LocalDate.parse(promotionInfo[3], DATE_TIME_FORMATTER);
+        LocalDate endDate = LocalDate.parse(promotionInfo[4], DATE_TIME_FORMATTER);
+
+        return Promotion.of(name, buy, get, startDate, endDate);
+    }
+
+    public static Product lineToProduct(final String line, final Promotions promotions) {
+        String[] productInfo = line.split(COMMA);
+        String name = productInfo[0];
+        int price = Integer.parseInt(productInfo[1]);
+        int quantity = Integer.parseInt(productInfo[2]);
+        String promotionName = productInfo[3];
+        Promotion promotion = promotions.findPromotionByName(promotionName);
+
+        return new Builder(name, quantity).price(price).promotion(promotion).build();
+    }
+}
--- java-convenience-store-7/src/main/java/store/util/Translator.java
+++ java-convenience-store-7/src/main/java/store/util/Translator.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Translator {
+public final class Translator {
     public static LocalDate getCurLocalDate() {
         LocalDateTime now = DateTimes.now();
         return now.toLocalDate();
