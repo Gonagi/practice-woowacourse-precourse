e07c70c6b333603d8bd3056b7c41c90c3677c96e
김건학 <kkh10002@naver.com>
2024-12-12 11:04:04 +0900

    feat(Store): 상품 구매 기능 구현
    프로모션 적용 여부에 따라 상품을 구매하고 영수증을 업데이트합니다.
    프로모션이 가능하면 프로모션, 일반 상품을 구매합니다.
    프로모션이 가능하지 않으면 일반 상품을 구매합니다.

---
+++ java-convenience-store-7/src/main/java/store/domain/Store.java
@@ -0,0 +1,46 @@
+package store.domain;
+
+public class Store {
+    private final Receipt receipt;
+    private final Storage storage;
+
+    private Store(final Storage storage) {
+        this.receipt = new Receipt();
+        this.storage = storage;
+    }
+
+    public static Store from(final Storage storage) {
+        return new Store(storage);
+    }
+
+    public void buyProduct(final Product product) {
+        if (product.checkPromotionDate()) {
+            buyPromotionAndBasicProducts(product);
+            return;
+        }
+        buyBasicProducts(product);
+    }
+
+    private void buyBasicProducts(Product product) {
+        Product basicProduct = findBasicProduct(product);
+        receipt.update(basicProduct);
+    }
+
+    private void buyPromotionAndBasicProducts(Product product) {
+        Product promotionProduct = findPromotionProduct(product);
+        Product basicProduct = findBasicProduct(product);
+        receipt.update(promotionProduct, basicProduct);
+    }
+
+    private Product findPromotionProduct(final Product findProduct) {
+        return storage.findPromotionProduct(findProduct);
+    }
+
+    private Product findBasicProduct(final Product findProduct) {
+        return storage.findBasicProduct(findProduct);
+    }
+
+    public Receipt getReceipt() {
+        return receipt;
+    }
+}
